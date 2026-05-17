00e2b802a8e57caa4772c7162fe88f16b8abd0a1
komal682 <prkomal682@gmail.com>
2026-05-17 15:24:16 +0530

    Fix import os error in settings

--- backend/settings.py
+++ backend/settings.py
@@ -6,6 +6,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
